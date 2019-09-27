74008104a9a86d8cc1ec2e0f53d29b61c2ea644a
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-09-27 10:21:44 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -7,19 +7,11 @@ class String
   end
 
   def question?
-    if self.end_with?("?")
-      return true
-    else
-      return false
-    end
+    self.end_with?("?")
   end
 
   def exclamation?
-    if self.end_with?("!")
-      return true
-    else
-      return false
-    end
+    self.end_with?("!")
   end
 
   def count_sentences
